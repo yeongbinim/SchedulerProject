82d0bdbd9498baf66bdc134887e31de01f483a93
yeongbinim <yeim.dev@gmail.com>
2024-12-06 15:18:16 +0900

    feat(lv3): JdbcTemplateRepository 구현
    - ScheduleRepository가 MemberRepository에 의존

---
+++ sql/member_insert.sql
@@ -0,0 +1,6 @@
+INSERT INTO member (name, password, email)
+VALUES ('John Doe', 'password123', 'aa@aa.aa'),
+       ('Jane Smith', 'password456', 'bb@bb.bb'),
+       ('Alice Johnson', 'password789', 'cc@cc.cc'),
+       ('Bob Brown', 'password101', 'dd@dd.dd'),
+       ('Charlie Davis', 'password102', 'ee@ee.ee');
